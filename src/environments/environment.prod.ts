551f548965bb0839c667b554209d10a51f4fbd55
Mathieu Jeanmougin <jeanmougin.mat@gmail.com>
2019-10-07 20:09:16 +0200

    'fix(url)'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.avalon-back.sopretty.me/',
+  apiUrl: 'https://avalon-back.sopretty.me/',
   endpoints: {
     createGame: 'games'
   }
